909f84e9c71f786b3b94f014e2c09c106ee98943
Carlos Garnacho <carlos@imendio.com>
2008-08-19 15:55:39 +0000

    Restore accidental revert of fix in r2093.
    2008-08-19  Carlos Garnacho  <carlos@imendio.com>
    
            * src/tracker-indexer/tracker-indexer-module.c: Restore accidental
            revert of fix in r2093.
    
    svn path=/branches/indexer-split/; revision=2105

--- src/tracker-indexer/tracker-indexer-module.c
+++ src/tracker-indexer/tracker-indexer-module.c
@@ -146,8 +146,18 @@ tracker_indexer_module_file_get_uri (GModule      *module,
 
 		return TRUE;
 	} else {
+		g_warning ("Could not get URI for '%s'\n", file->path);
+
 		g_free (tmp_dirname);
 		g_free (tmp_basename);
+
+		if (dirname) {
+			*dirname = NULL;
+		}
+
+		if (basename) {
+			*basename = NULL;
+		}
 
 		return FALSE;
 	}

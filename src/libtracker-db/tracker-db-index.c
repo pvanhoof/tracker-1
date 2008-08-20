9efb0f6be275cbeafbebcb446483232a779fbde9
Philip Van Hoof <pvanhoof@src.gnome.org>
2008-08-20 12:46:30 +0000

    Merge conflict fix
    svn path=/branches/indexer-split/; revision=2111

--- src/libtracker-db/tracker-db-index.c
+++ src/libtracker-db/tracker-db-index.c
@@ -857,6 +857,7 @@ tracker_db_index_flush (TrackerDBIndex *index)
 						     cache_flush_foreach, 
 						     priv->index);
 		} else {
+			g_warning ("Could not open index, cache was not flushed");
 			size = 0;
 		}
 	}

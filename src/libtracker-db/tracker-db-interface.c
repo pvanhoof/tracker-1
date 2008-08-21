d530be1a6de6b6b7e173957e5169203a9b2f2d11
Mikael Ottela <mottela@src.gnome.org>
2008-08-21 14:22:37 +0000

    Added row checking for result_set_get_value to prevent crashing
    svn path=/branches/indexer-split/; revision=2134

--- src/libtracker-db/tracker-db-interface.c
+++ src/libtracker-db/tracker-db-interface.c
@@ -520,7 +520,22 @@ _tracker_db_result_set_get_value (TrackerDBResultSet *result_set,
 
 	if (priv->col_types[column] != G_TYPE_INVALID) {
 		g_value_init (value, priv->col_types[column]);
-		fill_in_value (value, row[column]);
+		if (row) {
+			fill_in_value (value, row[column]);
+		} else {
+			/* Make up some empty value. */
+			switch (G_VALUE_TYPE (value)) {
+			case G_TYPE_INT:
+				g_value_set_int (value, 0);
+				break;
+			case G_TYPE_DOUBLE:
+				g_value_set_double (value, 0.0);
+				break;
+			case G_TYPE_STRING:
+				g_value_set_string (value, "");
+				break;
+			}
+		}
 	} else {
 		/* Make up some empty value */
 		g_value_init (value, G_TYPE_STRING);

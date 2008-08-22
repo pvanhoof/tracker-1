12f5d75544d6df44c6af9b4a7c8f8c8cb3639edf
Carlos Garnacho <carlos@imendio.com>
2008-08-22 10:14:05 +0000

    Reduce the warning to a debug message, it's still nice to have that info,
    2008-08-22  Carlos Garnacho  <carlos@imendio.com>
    
            * src/tracker-indexer/tracker-indexer-module.c
            (tracker_indexer_module_file_get_uri): Reduce the warning to a debug
            message, it's still nice to have that info, but it isn't crucial
            either.
    
    svn path=/branches/indexer-split/; revision=2140

--- src/tracker-indexer/tracker-indexer-module.c
+++ src/tracker-indexer/tracker-indexer-module.c
@@ -146,7 +146,7 @@ tracker_indexer_module_file_get_uri (GModule      *module,
 
 		return TRUE;
 	} else {
-		g_warning ("Could not get URI for '%s'\n", file->path);
+		g_debug ("Could not get URI for '%s'\n", file->path);
 
 		g_free (tmp_dirname);
 		g_free (tmp_basename);
